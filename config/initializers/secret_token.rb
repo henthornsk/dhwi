2171ff33e846d501dda89124980ef11e53e674ba
Susan Henthorn <henthorns@berea.edu>
2013-01-09 10:18:37 -0500

    Add all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dhwi2::Application.config.secret_token = '6169f226af5f782086a9daababce3eb1644c7bb4b106c6947e9fc54e4600e335bbb4d66af918875d90e4478c2594209f7ffd0b0c3db201d27d14992a29bbd785'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dhwi2::Application.config.session_store :cookie_store, key: '_dhwi2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dhwi2::Application.config.session_store :active_record_store
