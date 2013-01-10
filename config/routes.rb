8bfdbdc37ce8ebbac048c6d7ad0dbac17678f806
henthornsk <henthorns@berea.edu>
2013-01-10 16:47:10 -0500

    Added home page as default

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Dhwi2::Application.routes.draw do
   get "pages/about"
+  get "pages/index"
 
   resources :votes
 
@@ -56,7 +57,7 @@ Dhwi2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'topics#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
